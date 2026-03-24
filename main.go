bf436b836b0bc84439a618f9a7900b59eef6b5ae
drewesk <drewesk@gmail.com>
2026-03-24 13:02:16 -0400

    added light styling and centering to initial TUI Prompt

--- main.go
+++ main.go
@@ -1,22 +1,39 @@
 package main
 
 import (
-    "log"
-    tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/bubbles/textinput"
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
+	"log"
 )
 
+type Styles struct {
+	BorderColor lipgloss.Color
+	InputField  lipgloss.Style
+}
+
+func DefaultStyles() *Styles {
+	s := new(Styles)
+	s.BorderColor = lipgloss.Color("36")
+	s.InputField = lipgloss.NewStyle().BorderForeground(s.BorderColor).BorderStyle(lipgloss.NormalBorder()).Padding(1).Width(80)
+	return s
+}
+
 type model struct {
-	index int
-	questions []string
-	width int
-	height int
+	index       int
+	questions   []string
+	width       int
+	height      int
 	answerField textinput.Model
+	styles      *Styles
 }
 
 func New(questions []string) *model {
+	styles := DefaultStyles()
 	answerField := textinput.New()
-
-	return &model{questions: questions, answerField: answerField}
+	answerField.Placeholder = "Your answer here"
+	answerField.Focus()
+	return &model{questions: questions, answerField: answerField, styles: styles}
 }
 
 func (m model) Init() tea.Cmd {
@@ -24,28 +41,48 @@ func (m model) Init() tea.Cmd {
 }
 
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	var cmd tea.Cmd
 	switch msg := msg.(type) {
-		case tea.WindowSizeMsg:
-			m.width = msg.Width
-			m.height = msg.Height
-		case tea.KeyMsg:
-			switch msg.String() {
-				case "ctrl+c":
-					return m, tea.Quit
-			}
+	case tea.WindowSizeMsg:
+		m.width = msg.Width
+		m.height = msg.Height
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "ctrl+c":
+			return m, tea.Quit
+		case "enter":
+			m.index++
+			m.answerField.SetValue("done!")
+			return m, nil
+		}
 	}
-	return m, nil
+	m.answerField, cmd = m.answerField.Update(msg)
+	return m, cmd
 }
 
 func (m model) View() string {
 	if m.width == 0 {
 		return "loading.."
 	}
-	return lipgloss.JoinVertical(lipgloss.Center, m.questions[m.index], m.answerField.View())
+	return lipgloss.Place(
+		m.width,
+		m.height,
+		lipgloss.Center,
+		lipgloss.Center,
+		lipgloss.JoinVertical(
+			lipgloss.Center,
+			m.questions[m.index],
+			m.styles.InputField.Render(m.answerField.View()),
+		),
+	)
 }
 
 func main() {
-	questions := []string{"What is your name?", "What is my favorite TUI?", "What is your favorite quote?"}
+	questions := []string{
+		"What is your name?",
+		"What is my favorite TUI?",
+		"What is your favorite quote?",
+	}
 	m := New(questions)
 
 	f, err := tea.LogToFile("debug.log", "debug")
